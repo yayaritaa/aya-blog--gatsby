bc0760fca9c43f899cac3fec54903d46e3abf0cc
e10113831 <aya.ayari@vistaprint.com>
2020-04-28 16:42:17 +0100

    added link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
+import { Link } from "gatsby" // gatsby preload the page content that we are heading to when use this
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>It's Aya's blog</h1>
+      <p>
+        Need a developer? <Link to="/contact">Contact me.</Link>
+      </p>
+    </div>
+  )
+}
+
+export default IndexPage
